03d9095e8ed972c933d6414bf9c39380d9551edd
Tiarli Oliveira <tiarlioliveira@gmail.com>
2019-06-15 00:51:44 -0300

    Fix broken build

--- spec/customer_inviter_spec.rb
+++ spec/customer_inviter_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 module CustomerInviter
   RSpec.describe CustomerInviter do
     let(:instance) { described_class }
--- spec/unit/models/user_spec.rb
+++ spec/unit/models/user_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 module CustomerInviter
   RSpec.describe User do
     let(:instance) do
